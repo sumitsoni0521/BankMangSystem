365348aa985e18e08e6bf3586cbdb011a31b5050
Sumit Soni <139313696+sumitsoni0521@users.noreply.github.com>
2025-04-17 11:09:18 +0530

    Connn.java

--- src/bank/management/system/Connn.java
+++ src/bank/management/system/Connn.java
@@ -7,7 +7,7 @@ public class Connn {
     Statement statement;
     public Connn(){
         try{
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","@Host09server");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","");
             statement = connection.createStatement();
         }catch (Exception e){
             e.printStackTrace();
